0a44f4306dc5fd552802b978fae36fec2e77ade7
Shivakumar Burugu <shivakumar.burugu@usbank.com>
2019-05-21 13:54:09 +0530

    initial commit

---
+++ app/src/main/java/com/usb/proactivehelptest/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.usb.proactivehelptest
+
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+
+    val proactiveHelpHandler = Handler()
+    var proactiveRunnable:Runnable? = null
+    val PROACT_DURATION:Long = 5 * 1000
+    var lastInteractionTime = System.currentTimeMillis()
+    val TAG = MainActivity::class.java.simpleName
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        lastInteractionTime = System.currentTimeMillis()
+        proactiveRunnable = Runnable {
+            if(System.currentTimeMillis() - lastInteractionTime >= PROACT_DURATION) {
+                Log.d(TAG, "startproact help")
+                Toast.makeText(this, "Starting Proact Help", Toast.LENGTH_LONG).show()
+            } else {
+                proactiveHelpHandler.removeCallbacks(proactiveRunnable)
+                proactiveHelpHandler.postDelayed(proactiveRunnable,PROACT_DURATION)
+            }
+        }
+    }
+
+    override fun onUserInteraction() {
+        super.onUserInteraction()
+        lastInteractionTime = System.currentTimeMillis()
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        proactiveHelpHandler.postDelayed(proactiveRunnable,PROACT_DURATION)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        proactiveHelpHandler.removeCallbacks(proactiveRunnable)
+    }
+}
